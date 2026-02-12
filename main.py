7ea9025f0a75242863e0d925b60670bdfe0fa18e
Snapdragnn <niccrump98@gmail.com>
2026-02-12 23:56:48 +0000

    D: Player Movement

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = Clock.tick(60) / 1000
